e2412d9539be5b554dc2f0d6a2cd31318341d1fe
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-15 12:14:35 +0200

    Update

---
+++ 02-Data Base - Exercises/Ex 2 - Check Number Integer or Float .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type Any Number: ");
+            double num = double.Parse(Console.ReadLine());
+
+            if (num % 1 == 0)
+            {
+                Console.WriteLine("integer");
+            }
+            else
+            {
+                Console.WriteLine("floating-point");
+            }
+        }
+    }
+}
---
+++ 02-Data Base - Exercises/Ex 3 - Water Overflow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            int LitersInCountainer = 0;
+            int AllLiters = 0;
+            for (int i = 1; i <= rows; i++)
+            {
+                int NewLitersAdded = int.Parse(Console.ReadLine());
+
+                if (LitersInCountainer + NewLitersAdded <= 255)
+                {
+                    LitersInCountainer += NewLitersAdded;
+                }
+                
+                AllLiters += NewLitersAdded; 
+            }
+            if (AllLiters > 255)
+            {
+                Console.WriteLine("Insufficient capacity!");
+                Console.WriteLine(LitersInCountainer);
+            }
+            else
+            {
+                Console.WriteLine(LitersInCountainer);
+            }
+
+        }
+    }
+}
