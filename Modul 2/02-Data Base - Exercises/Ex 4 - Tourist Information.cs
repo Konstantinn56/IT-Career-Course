650e02b908fa8b43fb96c71c476d5a0bbace6d02
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-16 10:07:43 +0200

    Create Ex 4 - Tourist Information.cs

---
+++ Modul 2/02-Data Base - Exercises/Ex 4 - Tourist Information.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ImperialSys = Console.ReadLine().ToLower();//miles,inches,feet,yards,gallons
+            double value = double.Parse(Console.ReadLine());
+            double Converter = 0;
+            double ConvertedValue = 0;
+
+            switch (ImperialSys)
+            {
+                case "miles":
+                    ConvertedValue = value * 1.6;
+                    Console.WriteLine($"{value} miles = {ConvertedValue:F2} kilometers");
+                    break;
+
+                case "inches":
+                    ConvertedValue = value * 2.54;
+                    Console.WriteLine($"{value} inches = {ConvertedValue:F2} centimeters");
+                    break;
+
+                case "feet":
+                    ConvertedValue = value * 30;
+                    Console.WriteLine($"{value} feet = {ConvertedValue:F2} centimeters");
+                    break;
+
+                case "yards":
+                    ConvertedValue = value * 0.91;
+                    Console.WriteLine($"{value} yards = {ConvertedValue:F2} meters");
+                    break;
+
+                case "gallons":
+                    ConvertedValue = value * 3.8;
+                    Console.WriteLine($"{value} gallons = {ConvertedValue:F2} liters");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        
+    }
+}
