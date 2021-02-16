ef8751ed83153bee0c1db9151d28c052684e8e77
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-16 20:26:28 +0200

    New Exercises 16/02/2021

---
+++ Modul 2/02-Data Base - Exercises/Ex 6 - Catch The Thief.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string DataType = Console.ReadLine();//sbyte , int , long
+            int rows = int.Parse(Console.ReadLine());
+            double ThiefId = 0;
+            switch (DataType)
+            {
+                case "sbyte":
+                    ThiefId = sbyte.MinValue;
+                    for (int i = 0; i < rows; i++)
+                    {
+                        double id = double.Parse(Console.ReadLine());
+                        if (id > ThiefId && id <= sbyte.MaxValue)
+                        {
+                            ThiefId = id;
+                        }
+                    }
+                    break;
+
+                case "int":
+                    ThiefId = int.MinValue;
+                    for (int i = 0; i < rows; i++)
+                    {
+                        double id = double.Parse(Console.ReadLine());
+                        if (id > ThiefId && id <= int.MaxValue)
+                        {
+                            ThiefId = id;
+                        }
+                    }
+                    break;
+
+                case "long":
+                    ThiefId = long.MinValue;
+                    for (int i = 0; i < rows; i++)
+                    {
+                        double id = double.Parse(Console.ReadLine());
+                        if (id > ThiefId && id <= long.MaxValue)
+                        {
+                            ThiefId = id;
+                        }
+                    }
+                    break;
+
+                default:
+                    break;
+
+            }
+            Console.WriteLine(ThiefId);
+        }
+    }
+}
