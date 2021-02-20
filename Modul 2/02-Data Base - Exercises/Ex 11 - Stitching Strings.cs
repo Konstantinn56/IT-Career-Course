266d6674341d6896b19d12426747831721e98c07
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-20 18:17:22 +0200

    .

---
+++ Modul 2/02-Data Base - Exercises/Ex 11 - Stitching Strings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex_11___Stitching_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char Divider = char.Parse(Console.ReadLine());
+            string EvenOrOdd = Console.ReadLine();//even(cheten) , odd(necheten)
+            int rows = int.Parse(Console.ReadLine());
+            string TheString = "";
+
+            for (int i = 1; i <= rows; i++)
+            {
+                string NewWord = Console.ReadLine();
+                if (EvenOrOdd.Equals("even"))
+                {
+                    if(i % 2 == 0)
+                    {
+                        TheString += NewWord;
+                        TheString += Divider;
+                    }
+                }
+                else if (EvenOrOdd.Equals("odd"))
+                {
+                    if(i % 2 != 0)
+                    {
+                        TheString += NewWord;
+                        TheString += Divider;
+                    }
+                }
+            }
+           
+            Console.WriteLine(TheString);
+
+        }
+    }
+}
