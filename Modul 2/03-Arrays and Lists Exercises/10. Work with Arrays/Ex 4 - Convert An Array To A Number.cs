9c5eecf55a445e4d6fed4382b70600219a748e1c
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-20 21:05:24 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/10. Work with Arrays/Ex 4 - Convert An Array To A Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 10 3
+            //12 13
+            //25
+            int[] nums = Console.ReadLine()//2 10 3
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            while (nums.Length > 1)
+            {
+                int[] condensed = new int[nums.Length - 1];
+
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    condensed[i] = nums[i] + nums[i + 1];
+                }
+
+                nums = condensed;
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
