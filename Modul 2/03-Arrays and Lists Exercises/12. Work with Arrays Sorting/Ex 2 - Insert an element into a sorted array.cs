68730e588b5f17bb96c3afc0dd0368553cc26a49
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-02-28 20:38:11 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/12. Work with Arrays Sorting/Ex 2 - Insert an element into a sorted array.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace asdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Nums = Console.ReadLine()//1 2 3 7 9
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int num = int.Parse(Console.ReadLine());//4
+            int[] AllNums = new int[Nums.Length + 1];
+            int index = 0;
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                AllNums[i] = Nums[i];
+                index++;
+            }
+            AllNums[index] += num;
+
+            
+            for (int i = 0; i < AllNums.Length; i++)
+            {
+                int swapVar = AllNums[i];
+                int indexx = i;
+                while (indexx > 0 && AllNums[indexx - 1] >= swapVar)
+                {
+                    AllNums[indexx] = AllNums[indexx - 1];
+                    indexx--;
+                }
+                AllNums[indexx] = swapVar;
+
+            }
+            Console.WriteLine(string.Join(" ", AllNums));
+
+        }
+    }
+}
---
+++ Modul 2/03-Arrays and Lists Exercises/12. Work with Arrays Sorting/Ex 3 - Search for an element in a sorted array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace asdag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+            if (arr.Contains(num))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
