176d8563e91e0a6da9d01f89d9d90b68e4413d21
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-02 19:03:10 +0200

    .

--- Modul 2/03-Arrays and Lists Exercises/13. Lists Introduction/Ex 3 - List of names.cs
+++ Modul 2/03-Arrays and Lists Exercises/13. Lists Introduction/Ex 3 - List of names.cs
@@ -8,11 +8,17 @@ namespace sfgh
     {
         static void Main(string[] args)
         {
-            List<string> names = Console.ReadLine().Split().ToList();
-            //Ivan Maria Dimitar Simona Petya
+            List<string> names = Console.ReadLine().Split(", ").ToList();
+            //Ivan Dimitrov, Maria Ivanova, Dimitar Petrov
+           
             names.Reverse();
-            Console.WriteLine(string.Join("; ",names));
 
+            for (int i = 0; i < names.Count; i++)
+            {
+                List<string> reversedNames = names[i].Split().ToList();
+                reversedNames.Reverse();
+                Console.WriteLine(string.Join(" ", reversedNames));
+            }
         }
     }
 }
