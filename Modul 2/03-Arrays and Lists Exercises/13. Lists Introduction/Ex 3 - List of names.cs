ed19d925bea52103e953b99ccb5707e45c90600b
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-02 18:55:55 +0200

    Create Ex 3 - List of names.cs
    .

---
+++ Modul 2/03-Arrays and Lists Exercises/13. Lists Introduction/Ex 3 - List of names.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sfgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            //Ivan Maria Dimitar Simona Petya
+            names.Reverse();
+            Console.WriteLine(string.Join("; ",names));
+
+        }
+    }
+}
