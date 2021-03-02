6ba98f2719a561b4c393e08e9b14d0d3d4f91f0a
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-02 18:47:14 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/13. Lists Introduction/Ex 2 - Enter a list from the console using 1 line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fjfjl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();//1 2 3 4 5 6
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"nums[{nums.IndexOf(num)}] = {num}");
+            }
+        }
+    }
+}
