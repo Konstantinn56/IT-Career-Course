7eac179156be18d2ea4eeb050863b8aeba5f2916
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-03 08:52:53 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/14. Crawl lists/Ex 2 - List of even numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //3 4 8 5 7 5 2 1
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if(nums[i] % 2 == 0)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+            }
+        }
+    }
+}
