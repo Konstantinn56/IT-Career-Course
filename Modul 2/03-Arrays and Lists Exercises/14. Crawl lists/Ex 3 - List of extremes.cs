888c0800a3cf3dacb64020f443ca1d4af2aa7f8e
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-03 09:25:59 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/14. Crawl lists/Ex 3 - List of extremes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dfgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //5 4 8 5 7 8 2 1
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+            int MinNumberCount = 0;
+            int MaxNumberCount = 0;
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                
+                if(nums[i] < minNumber)
+                {
+                    minNumber = nums[i];              
+                }
+                if(nums[i] > maxNumber)
+                {
+                    maxNumber = nums[i];
+                }
+            }
+            
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == minNumber)
+                {
+                    MinNumberCount++;
+                }
+                if (nums[i] == maxNumber)
+                {
+                    MaxNumberCount++;
+                }
+            }
+            if(minNumber == maxNumber)
+            {
+                for (int a = 0; a < MinNumberCount; a++)
+                {
+                    Console.Write($"{minNumber} ");
+                }
+            }
+            else
+            {
+                for (int a = 0; a < MinNumberCount; a++)
+                {
+                    Console.Write($"{minNumber} ");
+                }
+                for (int a = 0; a < MaxNumberCount; a++)
+                {
+                    Console.Write($"{maxNumber} ");
+                }
+            }
+
+        }
+    }
+}
---
+++ Modul 2/03-Arrays and Lists Exercises/14. Crawl lists/Ex 4 - Maximum sequence of identical numbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sdgsag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //3 4 4 5 5 5 2 2
+            int RepeatableNum = 0;
+            int RepeatableNumCount = 0;
+            
+            for (int i = 0; i < nums.Count; i++)
+            {
+                int CurrentNum = 0;
+                int CurrentNumCount = 0;
+                CurrentNum = nums[i];
+                for (int a = 0; a < nums.Count; a++)
+                {
+                    if(nums[a] == CurrentNum)
+                    {
+                        CurrentNumCount++;
+                    }
+                }
+                if(CurrentNumCount > RepeatableNumCount)
+                {
+                    RepeatableNumCount = CurrentNumCount;
+                    RepeatableNum = CurrentNum;
+                }
+            }
+            for (int i = 0; i < RepeatableNumCount; i++)
+            {
+                Console.Write($"{RepeatableNum} ");
+            }
+        }
+    }
+}
