ad5a97fd6d6012dd64c74fae74acbf9ef86a96d5
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-04 12:25:47 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/15. Lists Manipulations/Ex 3 - Removing Negative Nums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace hdfh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //7 -2 -10 1
+            List<int> NewNums = new List<int>();
+
+            int temp = 0;
+            for (int i = nums.Count-1; i >= 0; i--)
+            {
+                if(nums[i] >= 0)
+                {
+                    NewNums.Add(nums[i]);
+                    temp++;
+                }
+            }
+            if(temp != 0)
+            {
+                Console.WriteLine(string.Join(" ", NewNums));
+            }
+            else
+            {
+                Console.WriteLine("Empty");
+            }
+            
+        }
+    }
+}
