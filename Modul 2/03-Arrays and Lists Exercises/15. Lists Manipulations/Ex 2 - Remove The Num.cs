77359030b120a1ef2ffa1b231ee9aef3cfcdd376
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-04 12:09:37 +0200

    Create Ex 2 - Remove The Num.cs

---
+++ Modul 2/03-Arrays and Lists Exercises/15. Lists Manipulations/Ex 2 - Remove The Num.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace atjt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //3 4 1 5 1 5 2 1
+            int RepeatingTime = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] == nums[nums.Count - 1])
+                {
+                    RepeatingTime++;
+                }
+            }
+            int NewNumsLength = nums.Count - RepeatingTime;
+            int[] NewNums = new int[NewNumsLength];
+            int index = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if(nums[nums.Count - 1] != nums[i])
+                {
+                    NewNums[index] += nums[i];
+                    index++;
+                }
+            }
+            Console.WriteLine(string.Join(" ",NewNums));
+        }
+    }
+}
