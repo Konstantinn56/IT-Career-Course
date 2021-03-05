0014dabf26817b28f9f7c92606f77f838ec55e08
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-05 12:45:39 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/16. Sorting Lists/Ex 4 Number of numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace j_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                int SwapVar = nums[i];
+                int index = i;
+                while(index > 0 && nums[index - 1] > SwapVar)
+                {
+                    nums[index] = nums[index - 1];
+                    index--;
+                }
+                nums[index] = SwapVar;
+            }
+            
+            ///////////////////////////////////////////////////////////
+
+            while (nums.Count > 0)
+            {
+                int RepeatingCounter = 1;
+                int RepeatingNum = nums[0];
+                for (int i = 1; i < nums.Count; i++)
+                {
+                    if (nums[0] == nums[i])
+                    {
+                        RepeatingCounter++;
+                    }
+                }
+                nums.RemoveRange(0, RepeatingCounter);
+                Console.WriteLine($"{RepeatingNum} -> {RepeatingCounter}");
+            }
+                
+            
+        }
+    }
+}
