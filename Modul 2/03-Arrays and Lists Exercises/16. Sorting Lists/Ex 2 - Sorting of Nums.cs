a8ce107bebeca5631d15bbf0f885e35f9fb8f9e0
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-05 12:32:35 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/16. Sorting Lists/Ex 2 - Sorting of Nums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace oup_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> nums = Console.ReadLine().Split().Select(double.Parse).ToList();
+            //8 2 7 3
+            //0 1 2 3
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                double SwapVar = nums[i];
+                int index = i;
+
+                while(index > 0 && nums[index - 1] >= SwapVar)
+                {
+                    nums[index] = nums[index - 1];
+                    index--;
+                }
+                nums[index] = SwapVar;
+            }
+            Console.WriteLine(string.Join(" <= ",nums));
+        }
+    }
+}
---
+++ Modul 2/03-Arrays and Lists Exercises/16. Sorting Lists/Ex 3 Square Nums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ахсфх
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //3 16 4 5 6 8 9 
+            List<int> squareNums = new List<int>();
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (Math.Sqrt(nums[i]) == (int)Math.Sqrt(nums[i]))
+                {
+                    squareNums.Add(nums[i]);
+                }
+            }
+            for (int i = 0; i < squareNums.Count; i++)
+            {
+                int swapVar = squareNums[i];
+                int index = i;
+                while (index > 0 && squareNums[index - 1] >= swapVar)
+                {
+                    squareNums[index] = squareNums[index - 1];
+                    index--;
+                }
+                squareNums[index] = swapVar;
+
+            }
+            squareNums.Reverse();
+            Console.WriteLine(string.Join(" ", squareNums));
+        }
+    }
+}
