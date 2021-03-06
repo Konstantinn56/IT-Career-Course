f1b00fe7fde67b270b8a05ada4c7e68effc912f0
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-06 12:49:39 +0200

    .

---
+++ Modul 2/03-Arrays and Lists Exercises/17. Additional Exercises For Lists/Ex - Longest Increasing Subsequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace joik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                length[i] = 1;
+                prev[i] = -1;
+                //3 14 5 12 15 7 8 9 11 10 1
+                //0  1 2  3  4 5 6 7  8  9 10
+                for (int j = 0; j < i; j++)
+                {
+                    if(sequence[j] < sequence[i] && length[j] >= length[i])
+                    {
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
+                    }
+                }
+                if (length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
+                }
+            }
+            /////////////////////////////////////////
+            List<int> longestSequence = new List<int>();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSequence.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];           
+            }
+            longestSequence.Reverse();
+            Console.WriteLine(string.Join(" ",longestSequence));
+        }
+    }
+}
