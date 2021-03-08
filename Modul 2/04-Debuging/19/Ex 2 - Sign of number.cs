ccd3e63e6ce8b532ecf5b908dcd18e38bc01f6d6
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-08 10:43:57 +0200

    .

---
+++ Modul 2/04-Debuging/19/Ex 2 - Sign of number.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dgb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintSignOfNum(number);
+        }
+        static void PrintSignOfNum(int number)
+        {
+            if(number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
---
+++ Modul 2/04-Debuging/19/Ex 3  Print Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace kk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //3
+
+            PrintWholeTriangle(n);
+
+            //1
+            //1 2
+            //1 2 3
+            //1 2
+            //1
+        }
+        static void PrintFirstPart(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                PrintLine(1, i + 1);
+            }
+        }
+        static void PrintLastPart(int n)
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                PrintLine(1, i);
+            }
+            Console.WriteLine();
+        }
+        static void PrintWholeTriangle(int n)
+        {
+            PrintFirstPart(n);
+            PrintLastPart(n);
+        }
+        static void PrintLine(int start ,int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
