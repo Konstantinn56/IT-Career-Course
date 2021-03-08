da16149fb0b3b895b90b17a6b6cdb930775bf1b2
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-08 10:05:14 +0200

    Create Ex 1 - Empty Receipt.cs

---
+++ Modul 2/04-Debuging/19/Ex 1 - Empty Receipt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace asdasd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+        static void PrintReceipt()
+        {
+            PrintReceiptHeader();
+            PrintReceiptFundamental();
+            PrintReceiptEnd();
+        }
+        static void PrintReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+        static void PrintReceiptFundamental()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+        static void PrintReceiptEnd()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© BG");
+        }
+    }
+}
