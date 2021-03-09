3c147e87522f38381f3159d7c62fa95d40749066
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-09 11:31:28 +0200

    Create Ex 4 - Print Square.cs

---
+++ Modul 2/04-Debuging/19/Ex 4 - Print Square.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace jkl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintWholeSquare(n);
+            
+        }
+        static void PrintWholeSquare(int n)
+        {
+            PrintStartAndEndLine(n);
+            PrintMiddleLine(n);
+            PrintStartAndEndLine(n);
+        }
+        static void PrintStartAndEndLine(int end)
+        {
+            for (int i = 0; i < end * 2; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+        static void PrintMiddleLine(int n)
+        {
+            for (int i = 0; i < n/2; i++)
+            {
+                Console.Write("-");
+                for (int a = 0; a < n - 1; a++)
+                {
+                    Console.Write(@"\/");
+                }
+                Console.Write("-");
+                Console.WriteLine();
+            }
+        }
+    }
+}
