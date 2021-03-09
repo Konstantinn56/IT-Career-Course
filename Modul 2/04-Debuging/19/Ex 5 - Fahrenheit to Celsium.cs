d449948befa5bca13dfa00e03a7ea367e2f715d8
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-09 11:55:37 +0200

    .

---
+++ Modul 2/04-Debuging/19/Ex 5 - Fahrenheit to Celsium.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  Fahrenheit = double .Parse(Console.ReadLine());
+            double Celsium = FahrenheitToCelsius(Fahrenheit);
+            Console.WriteLine($"{Celsium:f2}");
+        }
+        static double FahrenheitToCelsius(double Fahrenheit)
+        {
+            double Celsium = (Fahrenheit - 32) * 5 / 9;
+            return Celsium;
+        }
+    }
+}
---
+++ Modul 2/04-Debuging/19/Ex 6 - Getting Triangle Area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dhgn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double Area = GetTriangleArea(width, height);
+            Console.WriteLine(Area);
+        }
+        static double GetTriangleArea(double width , double height)
+        {
+            double Area = width * height / 2;
+            return Area;
+        }
+    }
+}
