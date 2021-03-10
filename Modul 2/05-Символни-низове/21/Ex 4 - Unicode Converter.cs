d045d3ad2db87a5cc548b2bf7eda81b9b3866d16
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-10 13:52:14 +0200

    .

---
+++ Modul 2/05-Символни-низове/21/Ex 4 - Unicode Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dgbb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string uniStr = UniStrConverter(str.Length , str);
+            Console.WriteLine(uniStr);
+            
+        }
+        static string UniStrConverter(int strLength,string str)
+        {
+            string uniStr = null;
+            for (int i = 0; i < strLength; i++)
+            {
+                uniStr += String.Format("\\u{0:x4}" , (int)str[i]);
+            }
+            return uniStr;
+        }
+    }
+}
---
+++ Modul 2/05-Символни-низове/21/Ex 5 - Multiplication of Symbols.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace sdfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            string str1 = arr[0];
+            string str2 = arr[1];
+
+            int maxLength = Math.Max(str1.Length, str2.Length);
+            int minLength = Math.Min(str1.Length, str2.Length);
+
+            double sum = Multiplication(str1, str2, minLength);
+
+            string maxString = GetMaxString(str1.Length, str2.Length, str1, str2);
+
+            for (int i = minLength; i < maxLength; i++)
+            {
+                sum += maxString[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+        static double Multiplication(string str1, string str2, int minLength)
+        {
+            double sum = 0;
+            for (int i = 0; i < minLength; i++)
+            {
+                double temp = str1[i] * str2[i];
+                sum += temp;
+            }
+            return sum;
+        }
+        static string GetMaxString(int str1Length ,int str2Length, string str1 , string str2)
+        {
+            return str1Length > str2Length ? str1 : str2;
+        }
+    }
+}
