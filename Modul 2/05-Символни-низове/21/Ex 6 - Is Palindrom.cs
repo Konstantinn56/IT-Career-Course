df9323c4a923ea677295e644f00387df6d63fb8b
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-10 14:00:51 +0200

    Create Ex 6 - Is Palindrom.cs

---
+++ Modul 2/05-Символни-низове/21/Ex 6 - Is Palindrom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ndng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            //012345678910
+            //alenafanela
+            Console.WriteLine($"{IsPalindrom(word,word.Length)}");
+        }
+        static bool IsPalindrom(string word , int wordLength)
+        {
+
+            bool isPalindrom = true;
+            if(wordLength == 1)
+            {
+                isPalindrom = true;
+            }
+            else
+            {
+                for (int i = 0; i < wordLength / 2; i++)
+                {
+                    if (word[i].Equals(word[wordLength - 1 - i]))
+                    {
+                        isPalindrom = true;
+                    }
+                    else
+                    {
+                        isPalindrom = false;
+                        break;
+                    }
+                }
+            }
+            return isPalindrom;
+        }
+    }
+}
