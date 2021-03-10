7ac0896e81b31a899f5c050e9ea92fd124e4d916
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-10 10:49:52 +0200

    Create Ex 3 - Char Reversing.cs

---
+++ Modul 2/05-Символни-низове/21/Ex 3 - Char Reversing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sgb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            PrintReversedChr(word.Length, word);
+        }
+        static void PrintReversedChr(int Length, string word)
+        {
+            char[] reversedSymbols = new char[Length];
+            int index = 0;
+            for (int i = Length - 1; i >= 0; i--)
+            {
+                reversedSymbols[index] = word[i];
+                index++;
+            }
+            foreach (var symbols in reversedSymbols)
+            {
+                Console.Write(symbols);
+            }
+        }
+    }
+}
