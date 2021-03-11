b86f86b43d6626bcab0c9ca1c39e421530bb32e5
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-11 17:56:03 +0200

    .

---
+++ Modul 2/05-Символни-низове/23/Ex 1 - Shake it.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sdfgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string pattern = Console.ReadLine();
+            //astalavista baby
+            //sta
+            while (word.Contains(pattern))
+            {
+                int startIndex = word.IndexOf(pattern);
+                word = word.Remove(startIndex, pattern.Length);
+
+                int endIndex = word.LastIndexOf(pattern);
+                word = word.Remove(endIndex,pattern.Length);
+
+                pattern = pattern.Remove(pattern.Length / 2, 1);
+                Console.WriteLine("Shake it");
+            }
+            Console.WriteLine("No Shake");
+            Console.WriteLine(word);
+
+        }
+        
+
+            
+    }
+}
