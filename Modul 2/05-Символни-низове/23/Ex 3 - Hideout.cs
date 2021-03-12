360f10169fbc1d0b145f17f89d1e24b5e9f5f637
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-12 11:31:54 +0200

    .

---
+++ Modul 2/05-Символни-низове/23/Ex 3 - Hideout.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace agadfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int startIndexOfHideout = 0;
+            int endIndexofHideout = 0;
+            int maxCount = 0;
+
+            while (maxCount == 0)
+            {
+                string[] symbolsAndLength = Console.ReadLine().Split();
+                char symbol = char.Parse(symbolsAndLength[0]);//@
+                int length = int.Parse(symbolsAndLength[1]);//5
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i].Equals(symbol))
+                    {
+                        int counter = 0;
+                        for (int a = i; a < text.Length; a++)
+                        {
+                            if (text[a].Equals(symbol))
+                            {
+                                counter++;
+                            }
+                            else
+                            {
+                                endIndexofHideout = a - 1;
+                                break;
+                            }
+                            if (counter >= length)
+                            {
+                                startIndexOfHideout = i;
+                                maxCount = counter;
+                            }
+                        }
+                    }
+                    if (maxCount >= length)
+                    {
+                        break;
+                    }
+                }
+            }
+            int hideoutSize = endIndexofHideout - startIndexOfHideout + 1;
+            Console.WriteLine($"Hideout found at index {startIndexOfHideout} and it is with size {hideoutSize}!");
+        }
+    }
+}
