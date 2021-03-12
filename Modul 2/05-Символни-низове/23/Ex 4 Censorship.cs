a3cc80829208466a40aa0d152fd1b948730ceabb
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-12 12:01:34 +0200

    .

---
+++ Modul 2/05-Символни-низове/23/Ex 4 Censorship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gjk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string banWord = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            if (text.Contains(banWord))
+            {
+                text = text.Replace(banWord, new string('*', banWord.Length));
+            }
+            Console.WriteLine(text);
+
+        }
+    }
+}
