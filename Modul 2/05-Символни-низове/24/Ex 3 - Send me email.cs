cf7a8134a2b8b6220b6a6e08f6d3c8bc7afa1674
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-13 12:03:20 +0200

    Create Ex 3 - Send me email.cs

---
+++ Modul 2/05-Символни-низове/24/Ex 3 - Send me email.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace ghjk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder eMail = new StringBuilder(Console.ReadLine());
+            //maria@abv.bg
+            int middleSymbolIndex = GetIndexFromMiddleSymbol(eMail.ToString());//5
+
+            int beforeSymbolSum = 0;
+            int afterSymbolSum = 0;
+            for (int i = 0; i < middleSymbolIndex; i++)
+            {
+                beforeSymbolSum += eMail[i];
+            }
+            for (int i = middleSymbolIndex + 1; i < eMail.Length; i++)
+            {
+                afterSymbolSum += eMail[i];
+            }
+            int diff = beforeSymbolSum - afterSymbolSum;
+            if(diff >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one.");
+            }
+
+        }
+        static int GetIndexFromMiddleSymbol(string eMail)
+        {
+            int index = 0;
+            for (int i = 0; i < eMail.Length; i++)
+            {
+                if (eMail[i].ToString().Equals("@"))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
