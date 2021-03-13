73f65b6cc3199c1ae7e8172a3409db20b7d02f30
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-13 11:05:29 +0200

    Create Ex 2 StringBuilder.cs

---
+++ Modul 2/05-Символни-низове/24/Ex 2 StringBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace dgbh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder(Console.ReadLine());
+            //That is not true.
+
+            string[] command = Console.ReadLine().Split();
+            //Remove 8 3
+
+            switch (command[0])
+            {
+                case "Remove":
+                    int index = int.Parse(command[1]);
+                    int length = int.Parse(command[2]);
+                    text = text.Remove(index , length);
+                    break;
+                
+                case "Append":
+                    text = text.Append(command[1]);
+                    break;
+                
+                case "Insert":
+                    int indexx = int.Parse(command[1]);
+                    text = text.Insert(indexx, command[2]);
+                    break;
+                
+                case "Replace":
+                    text = text.Replace(command[1], command[2]);
+                    break;
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
