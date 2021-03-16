c7805af07e62d14c628715363c0c13b2281cbc1e
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-16 09:20:06 +0200

    Create Ex 4 - Send me Email(Unicode).cs

---
+++ Modul 2/05-Символни-низове/24/Ex 4 - Send me Email(Unicode).cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+namespace dfghh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder eMail = new StringBuilder(Console.ReadLine());
+            //maria@abv.bg
+            int middleSymbolIndex = GetIndexFromMiddleSymbol(eMail.ToString());//5
+
+            string beforeSymbolSum = null;
+            string afterSymbolSum = null;
+            for (int i = 0; i < middleSymbolIndex; i++)
+            {
+                beforeSymbolSum += eMail[i];
+            }
+            for (int i = middleSymbolIndex + 1; i < eMail.Length; i++)
+            {
+                afterSymbolSum += eMail[i];
+            }
+
+            byte[] beforeSymbolBytes = Encoding.Unicode.GetBytes(beforeSymbolSum);
+            byte[] afterSymbolBytes = Encoding.Unicode.GetBytes(afterSymbolSum);
+
+            int beforeSymbolIntSum = 0;
+            int afterSymbolIntSum = 0;
+
+            foreach (var num in beforeSymbolBytes)
+            {
+                beforeSymbolIntSum += num;
+            }
+            foreach (var num in afterSymbolBytes)
+            {
+                afterSymbolIntSum += num;
+            }
+
+            int diff = beforeSymbolIntSum - afterSymbolIntSum;
+            if (diff >= 0)
+            {
+                Console.WriteLine("Call her!");
+            }
+            else
+            {
+                Console.WriteLine("She is not the one.");
+            }
+        }
+        
+        static int GetIndexFromMiddleSymbol(string eMail)
+        {
+            int index = 0;
+            for (int i = 0; i < eMail.Length; i++)
+            {
+                if (eMail[i].ToString().Equals("@"))
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
