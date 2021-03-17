2ccf6b2a00e39a7f11b24e25547016633a4732ea
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-17 13:07:10 +0200

    Create Ex 3 - Stings.cs

---
+++ Modul 2/08-Практически-изпит/10. Изпитни задачи Модул 2 2017-2018/Ex 3 - Stings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sdfgfsg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = Console.ReadLine();
+            //alyakeykala kalakey
+            //key
+
+            while (text.Contains(pattern) && pattern != (""))
+            {
+                int lastPaternIndex = text.LastIndexOf(pattern);
+                text = text.Remove(lastPaternIndex, pattern.Length);
+
+                pattern = pattern.Remove(pattern.Length / 2, 1);
+                if (pattern.Length > 1)
+                {
+                    string reversedPattern = null;
+                    for (int i = pattern.Length - 1; i >= 0; i--)
+                    {
+                        reversedPattern += pattern[i];
+                    }
+                    pattern = reversedPattern;
+                }
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
