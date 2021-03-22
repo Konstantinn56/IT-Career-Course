a2c98b5364aaf817912c3decb633ee001a18cb7d
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-22 13:08:11 +0200

    Create Ex 1 - Gym Equipment.cs

---
+++ Modul 2/08-Практически-изпит/11. Изпитни задачи Модул 2 2018-2019/Ex 1 - Gym Equipment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fgh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productsCount = int.Parse(Console.ReadLine());
+
+            int priceOfProducts = 0;
+
+            for (int i = 0; i < productsCount; i++)
+            {
+                string product = Console.ReadLine();
+
+                switch (product)
+                {
+                    case "exercise bike":
+                        priceOfProducts += 1789;
+                        break;
+                    case "treadmill":
+                        priceOfProducts += 5899;
+                        break;
+                    case "dumbbells":
+                        priceOfProducts += 579;
+                        break;
+                    case "cross trainer":
+                        priceOfProducts += 1699;
+                        break;
+
+                    default:
+                        Console.WriteLine("No product found!");
+                        i--;
+                        break;
+                }
+            }
+            Console.WriteLine($"{priceOfProducts:f2}");
+        }
+    }
+}
