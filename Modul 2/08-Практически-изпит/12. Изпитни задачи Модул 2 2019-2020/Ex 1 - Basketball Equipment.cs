540def82af66746cd3dd03dc6e44c526608d31c3
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-23 10:02:12 +0200

    Create Ex 1 - Basketball Equipment.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 1 - Basketball Equipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace asdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int basketballTrainingPrice = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            sum += basketballTrainingPrice;
+            double basketShoes = basketballTrainingPrice - (basketballTrainingPrice * 40/100);
+            double basketCloths = basketShoes - (basketShoes * 20/100);
+            double basketballBall = basketCloths /4;
+            double basketballAccsesoars = basketballBall /5;
+            sum += basketballBall + basketShoes + basketCloths + basketballAccsesoars;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
