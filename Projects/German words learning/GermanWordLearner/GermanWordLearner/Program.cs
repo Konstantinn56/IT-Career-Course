51e88677e41cc9171a3b70ff02117ff66126fe28
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-25 10:12:05 +0200

    .

---
+++ Projects/German words learning/GermanWordLearner/GermanWordLearner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GermanWordLearner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fromat: (german word)-(bulgarian word)");
+            Console.WriteLine("Type your words here:");
+
+            string[] words = Console.ReadLine().Split("-");
+
+            for (int i = 0; i < words.Length; i+=2)
+            {
+                Console.Write(words[i]);
+                Console.Write(" - ");
+                Console.Write(words[i+1]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
