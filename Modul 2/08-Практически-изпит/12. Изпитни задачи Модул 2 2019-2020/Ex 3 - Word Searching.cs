c9980881e92d11659eb0c98fc9160cbfe1829a1e
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-26 11:53:51 +0200

    Create Ex 3 - Word Searching.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 3 - Word Searching.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ghjkkk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordsCount = int.Parse(Console.ReadLine());
+
+            List<string> specialWords = new List<string>();
+            List<string> otherWords = new List<string>();
+
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string currentWord = Console.ReadLine();
+
+                if (IsTheWordStartWithUpperLetter(currentWord) && DoesTheWordContainsLetterI(currentWord) && IsTheWordLengthAbove7(currentWord))
+                {
+                    specialWords.Add(currentWord);
+                }
+                else
+                {
+                    otherWords.Add(currentWord);
+                }
+            }
+            PrintSpecialAndOtherWords(specialWords, otherWords);
+            
+        }
+        static void PrintSpecialAndOtherWords(List<string> specialWords, List<string> otherWords)
+        {
+            Console.WriteLine($"Special words: {string.Join(", ",specialWords)}");
+            Console.WriteLine($"Other words: {string.Join(", ",otherWords)}");
+        }
+        static bool IsTheWordStartWithUpperLetter(string currentWord)
+        {
+            bool isIt = false;
+
+            if(currentWord[0].ToString() == currentWord[0].ToString().ToUpper())
+            {
+                isIt = true;
+            }
+            return isIt;
+        }
+        static bool DoesTheWordContainsLetterI(string currentWord)
+        {
+            bool isIt = false;
+
+            if (currentWord.Contains("i") || currentWord.Contains("I"))
+            {
+                isIt = true;
+            }
+            return isIt;
+        }
+
+        static bool IsTheWordLengthAbove7(string currentWord)
+        {
+            bool isIt = false;
+            
+            if(currentWord.Length > 7)
+            {
+                isIt = true;
+            }
+            return isIt;
+        }
+    }
+}
