bb82302a5ee365bdcbefd97f54689c5951470cfb
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-30 10:01:23 +0300

    Create Ex 1 - maximum preparation.cs

---
+++ Modul 2/06-Многомерни-масиви/Ex 1 - maximum preparation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace gfhgfh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] oneRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = oneRow[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int maxSumRow = 0;
+            int maxSumCol = 0;
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+                    if(currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxSumRow = row;
+                        maxSumCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{matrix[maxSumRow,maxSumCol]} {matrix[maxSumRow, maxSumCol + 1]}");
+            Console.WriteLine($"{matrix[maxSumRow + 1, maxSumCol]} {matrix[maxSumRow + 1, maxSumCol + 1]}");
+        }
+    }
+}
