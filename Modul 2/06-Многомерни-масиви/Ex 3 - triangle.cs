f7afd5a7cbea350680973f1b6e9255a8f3e3e519
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-03-30 14:12:09 +0300

    Create Ex 3 - triangle.cs

---
+++ Modul 2/06-Многомерни-масиви/Ex 3 - triangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace adsff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            /*
+                       1
+                     1   1
+                   1   2   1
+                 1   3   3   1
+            */
+            long[][] triangle = new long[h][];
+
+            for (int row = 0; row < h; row++)
+            {
+                triangle[row] = new long[row + 1];
+            }
+
+            
+
+            triangle[0][0] = 1; 
+            for (int row = 0; row < h - 1; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    triangle[row + 1][col] += triangle[row][col];
+                    triangle[row + 1][col + 1] += triangle[row][col];
+                }
+            }
+
+            for (int row = 0; row < h; row++)
+            {
+                Console.Write("".PadLeft((h - row) *2));
+                for (int col = 0; col <=row; col++)
+                {
+                    Console.Write($"{triangle[row][col]}   ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
