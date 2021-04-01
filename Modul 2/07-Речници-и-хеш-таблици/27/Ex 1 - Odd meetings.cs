2b1b45a77243f52dc7cfa2ed859633745e072957
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-01 10:20:55 +0300

    .

---
+++ Modul 2/07-Речници-и-хеш-таблици/27/Ex 1 - Odd meetings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cbnnn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Select(w => w.ToLower()).ToArray();
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+
+            for (int word = 0; word < words.Length; word++)
+            {
+                if (!wordsCount.ContainsKey(words[word]))
+                {
+                    wordsCount.Add(words[word], 1);
+                }
+                else
+                {
+                    wordsCount[words[word]]++;
+                }
+            }
+
+            foreach (var w in wordsCount)
+            {
+                if(w.Value % 2 == 0)
+                {
+                    wordsCount.Remove(w.Key);
+                }
+                
+            }
+            Console.WriteLine(string.Join(", ", wordsCount.Keys));
+            
+
+        }
+    }
+}
