01c10ba2c69a654861acfa1cf87c2c161e5d86ec
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-01 11:41:15 +0300

    Create Ex 2 - Phonebook.cs

---
+++ Modul 2/07-Речници-и-хеш-таблици/27/Ex 2 - Phonebook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ghjhgj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            string[] currentCommand = Console.ReadLine().Split();
+            while (!currentCommand[0].Equals("END"))
+            {
+                switch (currentCommand[0])
+                {
+                    case "A":
+                        if (!phonebook.ContainsKey(currentCommand[1]))
+                        {
+                            phonebook.Add(currentCommand[1], currentCommand[2]);
+                        }
+                        else
+                        {
+                            phonebook[currentCommand[1]] = currentCommand[2];
+                        }
+                        break;
+                    
+                    case "S":
+                        if (!phonebook.ContainsKey(currentCommand[1]))
+                        {
+                            Console.WriteLine($"Contact {currentCommand[1]} does not exist.");
+                        }
+                        else
+                        {
+                            foreach (var n in phonebook)
+                            {
+                                if (n.Key.Equals(currentCommand[1]))
+                                {
+                                    Console.WriteLine($"{n.Key} -> {n.Value}");
+                                }
+                            }
+                            
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                currentCommand = Console.ReadLine().Split();
+            }
+        }
+    }
+}
