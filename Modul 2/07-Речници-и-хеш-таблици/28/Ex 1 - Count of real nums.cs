7f3a0800fc568cca4711a2991c1c0264ff76c3f7
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-02 10:56:03 +0300

    Create Ex 1 - Count of real nums.cs

---
+++ Modul 2/07-Речници-и-хеш-таблици/28/Ex 1 - Count of real nums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace asad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var n in nums)
+            {
+                if (!counts.ContainsKey(n))
+                {
+                    counts.Add(n, 1);
+                }
+                else
+                {
+                    counts[n]++;
+                }
+            }
+            foreach (var n in counts)
+            {
+                Console.WriteLine($"{n.Key} -> {n.Value}");
+            }
+        }
+    }
+}
