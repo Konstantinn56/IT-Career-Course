ada1bd6e896371cb16df3daef3f69163c18574fb
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-02 12:36:57 +0300

    .

---
+++ Modul 2/07-Речници-и-хеш-таблици/29/Ex 3 - Words Sorting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace aaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(". , : ; ( ) [ ] \" ' \\ / ! ?".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(w => w.ToLower())
+                .Where(w => w.Length < 5)
+                .Distinct()
+                .OrderBy(w => w)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",words));
+        }
+    }
+}
---
+++ Modul 2/07-Речници-и-хеш-таблици/29/Ex 4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace bbbb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //1 2 3 4 5 6 7 8
+            int k = nums.Length / 4;
+            int[] firstRowLeftSide = new int[k];
+            int[] firstRowRightSide = new int[k];
+            int[] firstRow = new int[2*k];
+            int[] secondRow = nums.Skip(k).Take(2*k).ToArray();
+
+            firstRowLeftSide = nums.Take(k).Reverse().ToArray();
+            firstRowRightSide = nums.TakeLast(k).Reverse().ToArray();
+
+            for (int i = 0; i < firstRowLeftSide.Length; i++)
+            {
+                firstRow[i] = firstRowLeftSide[i];
+            }
+            int index = 0;
+            for (int i = k; i < firstRow.Length; i++)
+            {
+                firstRow[i] = firstRowRightSide[index];
+                index++;
+            }
+
+            int[] sumArray = firstRow.Select((x, index) => x + secondRow[index]).ToArray();
+
+            Print(firstRow, secondRow, sumArray);
+        }
+        static void Print(int[] arr, int[]arr1,int[]arrResult)
+        {
+            Console.WriteLine($"{string.Join(" ", arr)} +");
+            Console.WriteLine($"{string.Join(" ", arr1)} =");
+            Console.WriteLine($"{string.Join(" ", arrResult)}");
+
+        }
+    }
+}
