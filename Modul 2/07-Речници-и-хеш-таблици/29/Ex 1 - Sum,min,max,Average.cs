e1b049317ae45455c3796a9cf0fc299cc5af1046
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-02 11:21:45 +0300

    .

---
+++ Modul 2/07-Речници-и-хеш-таблици/29/Ex 1 - Sum,min,max,Average.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace sfdggdfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] nums = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Sum = {nums.Sum()}");
+            Console.WriteLine($"Min = {nums.Min()}");
+            Console.WriteLine($"Max = {nums.Max()}");
+            Console.WriteLine($"Average = {nums.Average()}");
+        }
+    }
+}
---
+++ Modul 2/07-Речници-и-хеш-таблици/29/Ex 2 - MaxNums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dghjh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .OrderByDescending(X => X)
+                .Take(3)
+                .ToList();
+
+            Console.WriteLine($"{string.Join(" ",nums)}");
+        }
+    }
+}
