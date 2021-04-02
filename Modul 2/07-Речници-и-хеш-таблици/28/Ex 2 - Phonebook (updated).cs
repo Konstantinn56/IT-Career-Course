cb295096230ca742dce09c82341f63e36fbf88d9
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-02 11:02:53 +0300

    Create Ex 2 - Phonebook (updated).cs

---
+++ Modul 2/07-Речници-и-хеш-таблици/28/Ex 2 - Phonebook (updated).cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dsfgg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+
+            string[] currentCommand = Console.ReadLine().Split();
+            while (!currentCommand[0].Equals("END"))
+            {
+                switch (currentCommand[0])
+                {
+                    case "A":
+                        if (!phonebook.ContainsKey(currentCommand[1]))
+                        {
+                            phonebook.Add(currentCommand[1], currentCommand[2]);
+                        }
+                        else
+                        {
+                            phonebook[currentCommand[1]] = currentCommand[2];
+                        }
+                        break;
+
+                    case "S":
+                        if (!phonebook.ContainsKey(currentCommand[1]))
+                        {
+                            Console.WriteLine($"Contact {currentCommand[1]} does not exist.");
+                        }
+                        else
+                        {
+                            foreach (var n in phonebook)
+                            {
+                                if (n.Key.Equals(currentCommand[1]))
+                                {
+                                    Console.WriteLine($"{n.Key} -> {n.Value}");
+                                }
+                            }
+
+                        }
+                        break;
+
+                    case "ListAll":
+                        foreach (var n in phonebook)
+                        {
+                            Console.WriteLine($"{n.Key} -> {n.Value}");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                currentCommand = Console.ReadLine().Split();
+            }
+        }
+    }
+}
