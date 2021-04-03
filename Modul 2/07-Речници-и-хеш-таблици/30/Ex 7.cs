e4b75dbbe8010f8fde8b431a6290c613b1d1ac50
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-03 14:25:43 +0300

    Create Ex 7.cs

---
+++ Modul 2/07-Речници-и-хеш-таблици/30/Ex 7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adffghh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|');
+            //Country, <city,Population>
+            Dictionary<string, Dictionary<string, int>> report = new Dictionary<string, Dictionary<string, int>>();
+
+            while (!input[0].Equals("report"))
+            {
+                //Sofia|Bulgaria|1
+                //Veliko Tarnovo| Bulgaria | 2
+                string city = input[0];
+                string country = input[1];
+                int population = int.Parse(input[2]);
+                if (!report.ContainsKey(country))
+                {
+                    report.Add(country, new Dictionary<string, int>());
+                }
+                if (!report[country].ContainsKey(city))
+                {
+                    report[country].Add(city,0);
+                }
+                report[country][city] += population;
+
+                input = Console.ReadLine().Split('|');
+            }
+            foreach (var kvp in report.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                Console.WriteLine($"{kvp.Key} (total population: {kvp.Value.Values.Sum()})");
+                foreach (var item in kvp.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"=> {item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
