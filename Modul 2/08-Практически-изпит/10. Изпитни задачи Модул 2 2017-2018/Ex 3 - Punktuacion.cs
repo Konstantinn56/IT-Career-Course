de090335dc809d1e985ee7c6f0800e9f35b3b38e
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-03 10:09:53 +0300

    Create Ex 3 - Punktuacion.cs

---
+++ Modul 2/08-Практически-изпит/10. Изпитни задачи Модул 2 2017-2018/Ex 3 - Punktuacion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace okkoko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string chars = "!   #   %   &    (   )   *    -    .   :  ,  ?   @   [   ]   _    {   }";
+            char[] arr = chars.ToCharArray().Where(c => c != ' ').ToArray();
+
+
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.Length ; i++)
+            {
+                if (char.IsLower(input[i]))
+                {
+                    result.Append(input[i].ToString().ToUpper());
+                }
+                else if (char.IsUpper(input[i]))
+                {
+                    result.Append(input[i].ToString().ToLower());
+                }
+                else if (char.IsDigit(input[i]))
+                {
+                    int num = int.Parse(input[i].ToString());
+                    result.Append(num*2);
+                }
+                else if (arr.Contains(input[i]))
+                {
+                    result.Append(".");
+                }
+            }
+            string reversedResult = new string (result.ToString().Reverse().ToArray());
+            Console.WriteLine(reversedResult);
+        }
+    }
+}
