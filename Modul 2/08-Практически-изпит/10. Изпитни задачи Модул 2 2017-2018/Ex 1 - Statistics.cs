362b0ad22cb95c1ae89c0d3587943e714aacde72
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-03 09:17:53 +0300

    Create Ex 1 - Statistics.cs

---
+++ Modul 2/08-Практически-изпит/10. Изпитни задачи Модул 2 2017-2018/Ex 1 - Statistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fdghhh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int agentsCount = int.Parse(Console.ReadLine());
+            
+            decimal sum = 0;
+
+            for (int agent = 0; agent < agentsCount; agent++)
+            {
+                int dealsCount = int.Parse(Console.ReadLine());
+                for (int deal = 0; deal < dealsCount; deal++)
+                {
+                    int area = int.Parse(Console.ReadLine());
+                    decimal pricePerM = decimal.Parse(Console.ReadLine());
+
+                    decimal currentSum = area * pricePerM;
+                    sum += currentSum;
+
+                }
+
+            }
+            Console.WriteLine($"{sum/agentsCount:f3}");
+        }
+    }
+}
