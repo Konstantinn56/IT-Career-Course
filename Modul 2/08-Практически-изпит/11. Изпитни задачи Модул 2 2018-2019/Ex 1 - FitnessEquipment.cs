6b7c4015a20ebf87793112ef71aacbd8350bda3c
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-03 11:28:22 +0300

    Create Ex 1 - FitnessEquipment.cs

---
+++ Modul 2/08-Практически-изпит/11. Изпитни задачи Модул 2 2018-2019/Ex 1 - FitnessEquipment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace asdfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int productsCount = int.Parse(Console.ReadLine());
+
+            int treadmillPrice = 5899;
+            int crossTrainerPrice = 1699;
+            int exerciseBikePrice = 1789;
+            int dumbellsPrice = 579;
+            double sum = 0;
+            for (int i = 0; i < productsCount; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                switch (currentProduct)
+                {
+                    case "treadmill":
+                        sum += treadmillPrice;
+                        break;
+                    case "cross trainer":
+                        sum += crossTrainerPrice;
+                        break;
+                    case "exercise bike":
+                        sum += exerciseBikePrice;
+                        break;
+                    case "dumbbells":
+                        sum += dumbellsPrice;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
