20ca2af6bb538ae5f900c828fde28e5d9fbac7d7
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-07 19:12:48 +0300

    Create Ex 3 - Word searching.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 3 - Word searching.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sfggg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordsCount = int.Parse(Console.ReadLine());
+
+            List<string> specialWords = new List<string>();
+            List<string> otherWords = new List<string>();
+
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                if (DoesTheWordStartWithUpperLetter(word) && DoesTheWordContainsLetterI(word) && IsTheWordLengthAbove7(word))
+                {
+                    specialWords.Add(word);
+                }
+                else
+                {
+                    otherWords.Add(word);
+                }
+
+            }
+            Console.WriteLine(($"Special words: {string.Join(", ", specialWords)}"));
+            Console.WriteLine(($"Other words: {string.Join(", ", otherWords)}"));
+        }
+        static bool DoesTheWordStartWithUpperLetter(string word)
+        {
+            bool isTrue = false;
+            if(word[0].ToString().Equals(word[0].ToString().ToUpper()))
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+        static bool DoesTheWordContainsLetterI(string word)
+        {
+            bool isTrue = false;
+            if(word.Contains("i") || word.Contains("I"))
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+        static bool IsTheWordLengthAbove7(string word)
+        {
+            bool isTrue = false;
+            if(word.Length > 7)
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+    }
+}
