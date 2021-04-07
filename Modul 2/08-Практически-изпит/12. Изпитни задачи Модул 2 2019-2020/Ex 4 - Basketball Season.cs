206fe3f87091f4e16fb01f2df731c4187ce05f4c
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-07 19:41:35 +0300

    Create Ex 4 - Basketball Season.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 4 - Basketball Season.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace oijoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, int> playerNameAndPoints = new SortedDictionary<string, int>();
+            char[] charSeparators = new char[] {' ', '-'};
+            string[] commandLine = Console.ReadLine().Split(charSeparators, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            while (!commandLine[0].Equals("END"))
+            {
+                string name = commandLine[0];
+                int points = int.Parse(commandLine[1]);
+                if (!playerNameAndPoints.ContainsKey(name))
+                {
+                    playerNameAndPoints.Add(name, 0);
+                }
+                playerNameAndPoints[name] += points;
+
+                commandLine = Console.ReadLine().Split(charSeparators, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            }
+            foreach (var player in playerNameAndPoints)
+            {
+                Console.WriteLine($"{player.Key} -> {player.Value}");
+            }
+        }
+    }
+}
