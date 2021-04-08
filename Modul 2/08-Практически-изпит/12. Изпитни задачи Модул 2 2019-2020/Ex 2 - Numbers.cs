9d834c964d0012618b25a33e95c618fbe1482318
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-08 19:29:52 +0300

    Create Ex 2 - Numbers.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 2 - Numbers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nvmm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+            List<int> firstGroup = new List<int>();
+            List<int> secondGroup = new List<int>();
+            List<int> thirdGroup = new List<int>();
+            List<int> fourthGroup = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                if (DoesTheNumEndOn(num, 2, 4))
+                {
+                    firstGroup.Add(num);
+                }
+                else if (DoesTheNumEndOn(num, 5, 7))
+                {
+                    secondGroup.Add(num);
+                }
+                if (DoesTheNumSumEndOn(num, 3))
+                {
+                    thirdGroup.Add(num);
+                }
+                else if (DoesTheNumSumEndOn(num, 6))
+                {
+                    fourthGroup.Add(num);
+                }
+            }
+            PrintGroup(firstGroup);
+            PrintGroup(secondGroup);
+            PrintGroup(thirdGroup);
+            Console.WriteLine(string.Join(",", fourthGroup));
+
+        }
+        static void PrintGroup(List<int> group)
+        {
+            Console.WriteLine(string.Join(",",group));
+            Console.WriteLine();
+        }
+        static bool DoesTheNumEndOn(int num, int endingNum1, int endingNum2)
+        {
+            bool isTrue = false;
+            
+            if(num % 10 == endingNum1 || num % 10 == endingNum2)
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+        static bool DoesTheNumSumEndOn(int num, int endingNum)
+        {
+            bool isTrue = false;
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num = num / 10;
+            }
+            if(sum % 10 == endingNum)
+            {
+                isTrue = true;
+            }
+            return isTrue;
+        }
+        
+    }
+}
