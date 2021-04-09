c4d90c6635c64e5242f1473babac54d397ca66b2
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-09 20:37:20 +0300

    Create Ex 4 - ShoppingBill.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 4 - ShoppingBill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wretrwtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> productsNamesAndPrices = new Dictionary<string, double>();
+
+            string[] command = Console.ReadLine().Split('-');
+            double totalSum = 0;
+            while (!command[0].Equals("STOP SHOPPING"))
+            {
+                string product = command[0];
+                double productPirce = double.Parse(command[1]);
+                if (!productsNamesAndPrices.ContainsKey(product))
+                {
+                    productsNamesAndPrices.Add(product, 0.0);
+                }
+                productsNamesAndPrices[product] += productPirce;
+                totalSum += productPirce;
+                command = Console.ReadLine().Split('-');
+            }
+            foreach (var p in productsNamesAndPrices)
+            {
+                Console.WriteLine($"{p.Key} -> {p.Value:f2}");
+            }
+            Console.WriteLine($"Total sum: {totalSum:f2}");
+        }
+    }
+}
