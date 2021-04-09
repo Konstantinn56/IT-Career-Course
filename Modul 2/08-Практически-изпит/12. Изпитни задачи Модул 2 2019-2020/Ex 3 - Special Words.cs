ef37e2c587d93fefdb5e1e3e657d0420619f78fb
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-09 20:22:19 +0300

    Create Ex 3 - Special Words.cs

---
+++ Modul 2/08-Практически-изпит/12. Изпитни задачи Модул 2 2019-2020/Ex 3 - Special Words.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace okokk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordCount = int.Parse(Console.ReadLine());
+
+            List<string> specialWords = new List<string>();
+            List<string> otherWords = new List<string>();
+
+            for (int i = 0; i < wordCount; i++)
+            {
+                string word = Console.ReadLine();
+                if(word[0] != word[word.Length - 1] && word.Contains("e") && word.Length > 5)
+                {
+                    specialWords.Add(word);
+                }
+                else
+                {
+                    otherWords.Add(word);
+                }
+            }
+            Console.WriteLine($"Special words: {string.Join(", ",specialWords)}");
+            Console.WriteLine($"Other words: {string.Join(", ",otherWords)}");
+        }
+    }
+}
