c5d939942c7f3e607de52c3424ee492322aaff9c
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-11 10:14:27 +0300

    Create Ex 1.cs

---
+++ Modul 2/08-Практически-изпит/Изпитни задачи 2021/Ex 1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace asd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eggsCount = int.Parse(Console.ReadLine());
+            int red = 0;
+            int orange = 0;
+            int blue = 0;
+            int green = 0;
+            int maxEggCount = 0;
+            string maxEggColor = null;
+
+            for (int i = 0; i < eggsCount; i++)
+            {
+                string eggColor = Console.ReadLine();
+                switch (eggColor)
+                {
+                    case "red":
+                        red++;
+                        break;
+                    case "orange":
+                        orange++;
+                        break;
+                    case "blue":
+                        blue++;
+                        break;
+                    case "green":
+                        green++;
+                        break;
+                    default:
+                        break;
+                }
+                if(maxEggCount < red)
+                {
+                    maxEggCount = red;
+                    maxEggColor = "red";
+                }
+                else if (maxEggCount < orange)
+                {
+                    maxEggCount = orange;
+                    maxEggColor = "orange";
+                }
+                else if (maxEggCount < blue)
+                {
+                    maxEggCount = blue;
+                    maxEggColor = "blue";
+                }
+                else if (maxEggCount < green)
+                {
+                    maxEggCount = green;
+                    maxEggColor = "green";
+                }
+            }
+            Console.WriteLine($"Red eggs: {red}");
+            Console.WriteLine($"Orange eggs: {orange}");
+            Console.WriteLine($"Blue eggs: {blue}");
+            Console.WriteLine($"Green eggs: {green}");
+            Console.WriteLine($"Max eggs: {maxEggCount} -> {maxEggColor}");
+        }
+    }
+}
