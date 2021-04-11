21ab9ad97623bbf04c79c340dcce2f9acd6960a6
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-04-11 10:48:15 +0300

    Create Ex 4.cs

---
+++ Modul 2/08-Практически-изпит/Изпитни задачи 2021/Ex 4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex_4.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split('-');
+            double sum = 0.0;
+            Dictionary<string, double> productsList = new Dictionary<string, double>();
+
+            while (!command[0].Equals("Stop shopping"))
+            {
+                string product = command[0];
+                double price = double.Parse(command[1]);
+                sum += price;
+
+                if (!productsList.ContainsKey(product))
+                {
+                    productsList.Add(product, 0.0);
+                }
+                productsList[product] += price;
+
+                command = Console.ReadLine().Split('-');
+            }
+            foreach (var p in productsList.OrderBy(p => p.Value))
+            {
+                Console.WriteLine($"{p.Key} -> {p.Value:f2}");
+            }
+            Console.WriteLine($"Total sum:{sum:f2}");
+        }
+    }
+}
