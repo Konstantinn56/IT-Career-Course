58fe74089c0b0e2ba4b7627e9232fd3af1beb0c7
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-06 21:39:18 +0300

    .

---
+++ Modul 3/02-Полета-и-методи/04 06-04/Methods/Ex 5/People.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex_5
+{
+    public class People
+    {
+        private List<Person> peoples = new List<Person>();
+
+        public List<Person> Peoples
+        {
+            get { return peoples; }
+            set { peoples = value; }
+        }
+
+        public void AddPerson(Person person)
+        {
+            this.peoples.Add(person);
+        }
+        public People PeopleOver30(List<Person> people)
+        {
+            People peopleOver30 = new People();
+            foreach (var p in this.peoples.OrderBy(n => n.Name).Where(a => a.Age > 30))
+            {
+                peopleOver30.AddPerson(p);
+            }
+            return peopleOver30;
+        }
+    }
+}
---
+++ Modul 3/02-Полета-и-методи/04 06-04/Methods/Ex 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            People people = new People();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                Person person = new Person(name,age);
+                people.AddPerson(person);
+            }
+
+            People peopleOver30 = people.PeopleOver30(people.Peoples);
+
+            foreach (var p in peopleOver30.Peoples)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
