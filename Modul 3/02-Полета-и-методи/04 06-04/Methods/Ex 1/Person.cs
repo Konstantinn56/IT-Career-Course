6808a0129a69b5a1121b4ebe7895b825f55197af
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-06 08:24:18 +0300

    .

---
+++ Modul 3/02-Полета-и-методи/04 06-04/Methods/Ex 1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_1
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Im {name} {age} years old!");
+        }
+    }
+}
---
+++ Modul 3/02-Полета-и-методи/04 06-04/Methods/Ex 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            firstPerson.Name = "Gosho";
+            firstPerson.Age = 15;
+
+            firstPerson.IntroduceYourself();
+        }
+    }
+}
