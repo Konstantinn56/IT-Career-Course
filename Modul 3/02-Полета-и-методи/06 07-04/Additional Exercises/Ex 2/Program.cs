2b7773951d60a108df2ce5a4c7a51b1faa2cfa58
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-07 11:49:50 +0300

    .

--- Modul 3/02-Полета-и-методи/06 07-04/Additional Exercises/Ex 2/Program.cs
+++ Modul 3/02-Полета-и-методи/06 07-04/Additional Exercises/Ex 2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ex_2
 {
@@ -6,7 +8,48 @@ namespace Ex_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] info = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int count = info[0];
+            int overlapCount = info[1];
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] rInfo = Console.ReadLine().Split();
+                string id = rInfo[0];
+                int width = int.Parse(rInfo[1]);
+                int height = int.Parse(rInfo[2]);
+                int lX = int.Parse(rInfo[3]);
+                int lY = int.Parse(rInfo[4]);
+                Rectangle rectangle = new Rectangle(id, width, height, lX, lY);
+                rectangles.Add(rectangle);
+            }
+
+            for (int i = 0; i < overlapCount; i++)
+            {
+                string[] infoId = Console.ReadLine().Split();
+                string r1Id = infoId[0];
+                string r2Id = infoId[1];
+
+                Rectangle r1 = new Rectangle("",0,0,0,0);
+                Rectangle r2 = new Rectangle("",0,0,0,0);
+
+                foreach (var r in rectangles)
+                {
+                    if (r.Id.Equals(r1Id))
+                    {
+                        r1 = r;
+                    }
+                    if (r.Id.Equals(r2Id))
+                    {
+                        r2 = r;
+                    }
+                }
+
+                bool isTrue = Rectangle.DoTheRectanglesOverlap(r1, r2);
+                Console.WriteLine(isTrue);
+            }
         }
     }
 }
---
+++ Modul 3/02-Полета-и-методи/06 07-04/Additional Exercises/Ex 2/Rectangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_2
+{
+    public class Rectangle
+    {
+        private string id;
+        private int width;
+        private int height;
+        private int leftCornerX;
+        private int leftCornerY;
+        public Rectangle(string id, int width, int height, int leftCornerX, int leftCornerY)
+        {
+            Id = id;
+            Width = width;
+            Height = height;
+            LeftCornerX = leftCornerX;
+            LeftCornerY = leftCornerY;
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int LeftCornerX
+        {
+            get { return leftCornerX; }
+            set { leftCornerX = value; }
+        }
+        public int LeftCornerY
+        {
+            get { return leftCornerY; }
+            set { leftCornerY = value; }
+        }
+
+        static public bool DoTheRectanglesOverlap(Rectangle rec1, Rectangle rec2)
+        {
+            int r1LeftCornerX = rec1.leftCornerX;
+            int r1LeftCornerY = rec1.leftCornerY;
+            int r1BottomRightCornerX = r1LeftCornerX + rec1.Width;
+            int r1BottomRightCornerY = r1LeftCornerY - rec1.Height;
+
+            int r2LeftCornerX = rec2.leftCornerX;
+            int r2LeftCornerY = rec2.leftCornerY;
+            int r2BottomRightCornerX = r2LeftCornerX + rec2.Width;
+            int r2BottomRightCornerY = r2LeftCornerY - rec2.Height;
+
+            if(r1LeftCornerX >= r2BottomRightCornerX || r2LeftCornerX >= r1BottomRightCornerX)
+            {
+                return false;
+            }
+            if(r1LeftCornerY <= r2BottomRightCornerY || r2LeftCornerY <= r1BottomRightCornerY)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
