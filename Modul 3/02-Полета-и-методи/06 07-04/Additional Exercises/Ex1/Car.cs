2743383411349b40a5c66cdab37099dec1c0b0e3
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-07 09:32:53 +0300

    .

---
+++ Modul 3/02-Полета-и-методи/06 07-04/Additional Exercises/Ex1/Car.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    public class Car
+    {
+        private string model;
+        private double fuel;
+        private double fuelCostPerKm;
+        private int travelledDistance = 0;
+
+        public Car(string model, double fuel, double fuelCostPerKm)
+        {
+            this.model = model;
+            this.Fuel = fuel;
+            this.FuelCostPerKm = fuelCostPerKm;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public double Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+        public double FuelCostPerKm
+        {
+            get { return fuelCostPerKm; }
+            set { fuelCostPerKm = value; }
+        }
+        public int TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public void CarDrive(int km)
+        {
+            double fuelCostForThisKm = km * this.FuelCostPerKm;
+            if(this.Fuel > fuelCostForThisKm)
+            {
+                this.Fuel -= fuelCostForThisKm;
+                this.TravelledDistance += km;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{model} {fuel:f2} {travelledDistance}";
+        }
+    }
+}
---
+++ Modul 3/02-Полета-и-методи/06 07-04/Additional Exercises/Ex1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsCount = int.Parse(Console.ReadLine());
+
+            List<Car> carList = new List<Car>();
+
+            for (int i = 0; i < carsCount; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split();
+                string model = carInfo[0];
+                double fuel = double.Parse(carInfo[1]);
+                double fuelCost = double.Parse(carInfo[2]);
+                Car car = new Car(model,fuel,fuelCost);
+                carList.Add(car);
+            }
+
+            string[] info = Console.ReadLine().Split();
+            while (!info[0].Equals("End"))
+            {
+                string model = info[1];
+                int km = int.Parse(info[2]);
+                foreach (Car car in carList)
+                {
+                    if (model.Equals(car.Model))
+                    {
+                        car.CarDrive(km);
+                    }
+                }
+                info = Console.ReadLine().Split();
+            }
+
+            foreach (Car car in carList)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
