2e0c99182a103b4d1163f756b5f927dbe3c98634
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-10 19:18:22 +0300

    .

---
+++ Modul 3/04-Статични-полета-и-методи 10.04/Deffining classes/Ex1/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private static int counter;
+
+        public Person(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            counter++;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+            }
+        }
+        public static int Counter
+        {
+            get { return counter; }
+        }
+    }
+}
---
+++ Modul 3/04-Статични-полета-и-методи 10.04/Deffining classes/Ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("ASD",123);
+            Person person2 = new Person("ASDasd",112323);
+            Person person3 = new Person("ASasdD",123123);
+
+            Console.WriteLine(Person.Counter);
+        }
+    }
+}
