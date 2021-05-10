d3b6732cb5a95a5c0c083e3fd8a34b30adc255b6
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-05-10 10:06:05 +0300

    .

---
+++ Modul 3/03-Енкапсулация-на-данни/08/Acces modifiers/Ex1/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary = 0;
+
+        public Person(string firstName, string lastName, int age, double salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+        }
+
+        public void IncreaseSalary(double percent)
+        {
+            if(this.Age > 30)
+            {
+                this.salary += this.salary * percent / 100;
+            }
+            else
+            {
+                this.salary += this.salary * percent / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} get {this.salary:f2} leva";
+        }
+    }
+}
---
+++ Modul 3/03-Енкапсулация-на-данни/08/Acces modifiers/Ex2/Chicken.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex2
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name cannot be empty.");
+            }
+            this.name = name;
+            if (MinAge > age || MaxAge < age)
+            {
+                throw new Exception("Age should be between 0 and 15.");
+            }
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+        public int Age
+        {
+            get { return this.age;}
+        }
+
+
+
+        public double GetProductPerDay()
+        {
+            return this.CalculateProductPerDay();
+        }
+
+        private double CalculateProductPerDay()
+        {
+            switch (this.age)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    return 1.5;
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    return 2;
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    return 1;
+                default:
+                    return 0.75;
+            }
+        }
+    }
+}
