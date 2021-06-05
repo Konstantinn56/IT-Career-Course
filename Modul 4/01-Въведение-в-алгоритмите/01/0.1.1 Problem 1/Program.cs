1f609b96b6e82e3426c5f935f0f27263fe81d460
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-06-05 09:58:15 +0300

    .

---
+++ Modul 4/01-Въведение-в-алгоритмите/01/0.1.1 Problem 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _0._1._1_Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number = int.Parse(Console.ReadLine());
+            bool exist = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist)
+            {
+                Console.WriteLine($"{number} exists in the list");
+            }
+            else
+            {
+                Console.WriteLine($"{number} not exists in the list");
+            }
+        }
+    }
+}
---
+++ Modul 4/01-Въведение-в-алгоритмите/01/01.1 Problem 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._1_Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
