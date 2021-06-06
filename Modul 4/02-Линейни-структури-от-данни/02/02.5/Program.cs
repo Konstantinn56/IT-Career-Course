97ff61975ba5920e43e6f43bb795bb828d4e8783
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-06-06 12:09:45 +0300

    .

--- Modul 4/02-Линейни-структури-от-данни/02/02.5/Program.cs
+++ Modul 4/02-Линейни-структури-от-данни/02/02.5/Program.cs
@@ -6,7 +6,19 @@ namespace _02._5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReversedList<int> list = new ReversedList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
+            Console.WriteLine(list[3]);
+
+
         }
     }
 }
---
+++ Modul 4/02-Линейни-структури-от-данни/02/02.5/ReversedList.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02._5
+{
+    public class ReversedList<T> : IEnumerable<T>
+    {
+        private T[] items;
+        private const int initialCapacity = 2;
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+
+        public ReversedList(int capacity = initialCapacity)
+        {
+            this.Capacity = capacity;
+            this.items = new T[this.Capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if(index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return this.items[this.Count - 1 - index];
+            }
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                this.items[this.Count - 1 - index] = value;
+
+            }
+        }
+        public void Add(T item)
+        {
+            if(this.Count >= this.items.Length)
+            {
+                this.Grow();
+            }
+
+            this.items[this.Count] = item;
+            this.Count++;
+        }
+        public T RemoveAt(int index)
+        {
+            T element = this.items[this.Count - 1 - index];
+
+            this.items = this.items.Take(this.Count - 1 - index).ToArray();
+            this.items = this.items.Concat(this.items.Skip(this.Count - index)).ToArray();
+            this.Count--;
+            return element;
+        }
+
+        private void Grow()
+        {
+            this.Capacity *= 2;
+            T[] newItems = new T[this.Capacity];
+            for (int i = 0; i < this.Count; i++)
+            {
+                newItems[i] = this.items[i];
+            }
+            this.items = newItems;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Count-1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
