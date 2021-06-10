c4d9159a6ad90dcea1bdec4ce180524a178fcf94
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-06-10 20:59:11 +0300

    .

---
+++ Modul 4/02-Линейни-структури-от-данни/02 SelfMade/02.4/ArrayStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._4
+{
+    public class ArrayStack<T>
+    {
+        private T[] elements;
+        private int top = -1;
+        public int Count { get; private set; }
+        private const int DefaultCapacity = 4;
+        public int InitialCapacity { get; private set; }
+
+        public ArrayStack(int capacity = DefaultCapacity)
+        {
+            this.InitialCapacity = capacity;
+            this.elements = new T[capacity];
+        }
+
+        public void Push(T element) 
+        {
+            if(this.Count == this.InitialCapacity)
+            {
+                this.Grow();
+            }
+            this.elements[this.top + 1] = element;
+            this.Count++;
+            this.top++;
+        }
+        
+        
+        public T Pop()
+        {
+            T result = this.elements[this.top];
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            this.elements[this.top] = default(T);
+            this.top--;
+            this.Count--;
+            return result;
+        }
+        
+        public T[] ToArray()
+        {
+            T[] result = new T[this.Count];
+            for (int i = 0; i < this.Count; i++)
+            {
+                result[i] = this.elements[i];
+            }
+            return result;
+        }
+        
+        private void Grow()
+        {
+            this.InitialCapacity *= 2;
+            T[] copy = new T[this.InitialCapacity];
+            for (int i = 0; i < this.Count; i++)
+            {
+                copy[i] = this.elements[i];
+            }
+            this.elements = copy;
+        }
+
+
+    }
+}
---
+++ Modul 4/02-Линейни-структури-от-данни/02 SelfMade/02.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayStack<int> stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Pop();
+            int[] arr = stack.ToArray();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
