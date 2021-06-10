0de5bf66efb621d59b7a446c7138922539c75d53
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-06-10 16:49:53 +0300

    .

---
+++ Modul 4/02-Линейни-структури-от-данни/02 SelfMade/02.7/LinkedQueue.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02._7
+{
+    public class LinkedQueue<T>
+    {
+        public int Count { get; set; }
+        public QueueNode<T> Head { get; set; }
+        public QueueNode<T> Tail { get; set; }
+
+        public LinkedQueue()
+        {
+            this.Head = null;
+            this.Tail = null;
+        }
+
+        public void Enqueue(T element)
+        {
+            QueueNode<T> newNode = new QueueNode<T>(element);
+            if (this.Count == 0)
+            {
+                this.Head = newNode;
+            }
+            else
+            {
+                newNode.Prev = this.Tail;
+                this.Tail.Next = newNode;
+            }
+            this.Tail = newNode;
+            this.Count++;
+        }
+
+        public T Dequeue()
+        {
+            T element = this.Head.Element;
+
+            this.Head = this.Head.Next;
+            if (this.Head == null)
+            {
+                this.Tail = null;
+            }
+            else
+            {
+                this.Head.Prev = null;
+            }
+            this.Count--;
+            return element;
+        }
+
+  
+    }
+    public class QueueNode<T>
+    {
+        public T Element { get; set; }
+        public QueueNode<T> Next { get; set; }
+        public QueueNode<T> Prev { get; set; }
+
+        public QueueNode(T item)
+        {
+            this.Element = item;
+            this.Next = null;
+            this.Prev = null;
+        }
+    }
+}
