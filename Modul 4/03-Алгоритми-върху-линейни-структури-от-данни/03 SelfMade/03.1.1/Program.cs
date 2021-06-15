9b5b9b0e009817b362b3c3bf0c0e58e9555926ab
Konstantinn56 <75718282+Konstantinn56@users.noreply.github.com>
2021-06-15 21:12:27 +0300

    .

--- Modul 4/03-Алгоритми-върху-линейни-структури-от-данни/03 SelfMade/03.1.1/Program.cs
+++ Modul 4/03-Алгоритми-върху-линейни-структури-от-данни/03 SelfMade/03.1.1/Program.cs
@@ -10,7 +10,7 @@ namespace _03._1._1
             int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int num = int.Parse(Console.ReadLine());
 
-
+            InsertMethod(nums, num);
             
         }
         public static void NumberCheck(int[] nums,int numToCheck)
@@ -37,12 +37,44 @@ namespace _03._1._1
             }
 
             int[] newNums = new int[nums.Length];
-
+            int indexToInsert = 0;
             for (int i = 0; i < nums.Length; i++)
             {
+                if (i == nums.Length -1)
+                {
+                    if(nums[i] < numToInsert)
+                    {
+                        indexToInsert = i + 2;
+                        break;
+                    }
+                }
+                if (nums[0] >= numToInsert)
+                {
+                    indexToInsert = 0;
+                    break;
+                }
+                if (nums[i] <= numToInsert && nums[i + 1] >= numToInsert)
+                {
+                    indexToInsert = i + 1;
+                    break;
+                }
             }
 
+            for (int i = 0; i < indexToInsert; i++)
+            {
+                newNums[i] = oldNums[i];
+            }
+            newNums[indexToInsert] = numToInsert;
+            for (int i = indexToInsert + 1; i < newNums.Length; i++)
+            {
+                newNums[i] = oldNums[i - 1];
+            }
+            newNums[newNums.Length - 1] = oldNums[oldNums.Length - 1];
 
+            foreach (var item in newNums)
+            {
+                Console.Write($"{item} ");
+            }
 
         }
     }
